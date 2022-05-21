986bd5e391e6b3045668f5dfd2a9a3869012ab68
Danglich <86116972+Danglich@users.noreply.github.com>
2022-05-21 10:16:18 +0700

    first commit

--- main.js
+++ main.js
@@ -1,23 +1,58 @@
 
-const $ = document.querySelector.bind(document);
 
+const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
+const headApp = $('.play-music')
 const musicList = $('.music-list')
+const cdImg = $('.play-music__img')
+const playBtn = $('.play-music__control-playing')
+const audio = $('#audio')
+const time = $('.play-music__time')
+const loopBtn = $('.play-music__control-item.loop')
+const nextBtn = $('.play-music__control-item.next')
+const prevBtn = $('.play-music__control-item.prev')
+const randomBtn = $('.play-music__control-item.random')
 
-const cdPlayHeader = $('.play-music')
 
 
+const animateCd = cdImg.animate([{transform: 'rotate(360deg)'}], {
+    duration: 10000,
+    iteration: Infinity
+})
 
-
-
-
+animateCd.pause()
 
 
 
 const app = {
-    
+    isRandom : false,
+    isPlaying : false,
+    isLoop : false,
+    currentIndex: 0,
     songs : [
+        
+        { 
+            id: 12,
+            name: "Stay",
+            single: "The Kid LAROI ft. Justin Bieber",
+            image: "./assets/img/song12.jpg",
+            music : "./assets/musics/song12.mp3"
+        },
+        { 
+            id: 11,
+            name: "Dusk Till Dawn",
+            single: "ZAYN ft. Sia",
+            image: "./assets/img/song11.jpg",
+            music : "./assets/musics/song11.mp3"
+        },
+        { 
+            id: 13,
+            name: "Mood",
+            single: "Mood ft. Iann dior - Ro Ryon",
+            image: "./assets/img/song13.jpg",
+            music : "./assets/musics/song13.mp3"
+        },
         { 
             id: 1,
             name: "Một phút",
@@ -101,12 +136,12 @@ const app = {
         
     ],
 
-
-    handlerLoadSongs: function() {
-        var htmls = this.songs.map(song => {
+    render : function() {
+        var htmls = [];
+        htmls = this.songs.map(function(song, index) {
             return `
-            <li class="music-item" onclick="app.handlerSelection(${song.id})">
-                <div style=" background-image: url('${song.image}')" class="music-item__img">
+            <li class="music-item" data-index = "${index}">
+                <div  style=" background-image: url('${song.image}')" class="music-item__img">
 
                 </div>
                 <div class="music-item__text">
@@ -117,135 +152,38 @@ const app = {
                     <i class="fa-solid fa-ellipsis"></i>
                 </div>
             </li>
-            
             `
-        });
+        })
 
         musicList.innerHTML = htmls.join('')
 
     },
 
     
-    
-    
-    getSongById: function(id) {
-        var song = this.songs.find(s => s.id === id);
+    loadSongCurrent: function() {
+        const song = this.songs[this.currentIndex]
+        const nameNode = $('.play-music__header-name')
+        const imageNode = $('.play-music__img')
+        const songItems = $$('.music-item')
+        const songItem = $(`li[data-index = "${this.currentIndex}"]`)
         
-        return song
-        
+        songItems.forEach(function(songItem) {
+            songItem.classList.remove('active')
+        })
+        songItem.classList.add('active')
+        songItem.scrollIntoView({behavior: "smooth", block: "center", inline: "nearest"})
+        nameNode.textContent = song.name
+        imageNode.style.backgroundImage = `url('${song.image}')`
+        audio.src = song.music
+
     },
 
-
-    
-    handlerSelection: function(id) {
-        var song = this.getSongById(id);
+    handleEvents: function() {
+        const _this = this;
+        //Khi kéo màn hình
+        const cdHeight = cdImg.clientHeight;
+        const cdWidth = cdImg.clientWidth;
         
-        cdPlayHeader.innerHTML = `
-        <div class="play-music__header">
-        <span class="play-music__header-title">Now playing: </span>
-        <h2 class="play-music__header-name">${song.name}</h2>
-        </div>
-        
-        <div style="background-image: url('${song.image}')" class="play-music__img">
-        
-        </div>
-        
-        <div class="play-music__control">
-        
-        <li class="play-music__control-item">
-        <i class="play-music__control-icon fa-solid fa-rotate-right"></i>
-        
-        </li>
-        
-        <li class="play-music__control-item">
-        <i class="play-music__control-icon fa-solid fa-backward-fast"></i>
-        
-        </li>
-        
-        <li onclick="app.controlPlaying()" class="play-music__control-item play-music__control-playing pause">
-                    <i class="play-music__control-icon play-music__control-icon--pause fa-solid fa-play"></i>
-                    <i class="play-music__control-icon play-music__control-icon--playing fa-solid fa-circle-pause"></i>
-                    
-
-        </li>
-        
-        
-        
-        <li class="play-music__control-item">
-        <i class="play-music__control-icon fa-solid fa-forward-fast"></i>
-        
-        </li>
-        
-        <li class="play-music__control-item">
-        <i class="play-music__control-icon fa-solid fa-shuffle"></i>
-        </li>
-        
-        </div>
-        
-        
-        <input type="range" name="" id="" class="play-music__time" step = 0.1 min=0 max=100 value = 0>
-        
-        <audio id="audio" src="${song.music}"></audio>
-        
-        `;
-
-        
-
-        this.handleScrollApp()
-
-        
-
-
-        
-    },
-
-
-
-    controlPlaying: function () {
-        var btnControl = $('.play-music__control-playing')
-        var isPlaying = $('.play-music__control-playing.playing')
-        var audio = document.getElementById('audio')
-        if (!isPlaying){
-            btnControl.classList.remove('pause')
-            btnControl.classList.add('playing')
-            audio.play()
-            
-
-        }
-        else{
-            btnControl.classList.remove('playing')
-            btnControl.classList.add('pause')
-            audio.pause()
-        }
-
-        var duration = audio.duration;//chiều dài của audio
-
-        var navTime = $('.play-music__time')
-        
-        
-        audio.ontimeupdate = function(){
-            //audio.currentTime = navTime.attributes['value'] / 100;
-            navTime.setAttribute('value', (audio.currentTime / duration)*100)
-
-
-        }
-
-        
-    },
-
-    handleControlTime: function () {
-        var audio = document.getElementById('audio')
-        
-    },
-
-
-
-    handleScrollApp: function() {
-
-        let cd = $('.play-music__img');
-        const cdHeight = cd.clientHeight;
-        const cdWidth = cd.clientWidth;
-
         window.onscroll = function(){
             const scrollPx =  document.documentElement.scrollTop;
             var newHeight = cdHeight - scrollPx;
@@ -253,59 +191,168 @@ const app = {
                 newHeight = 0
             }
 
-            cd.style.height = newHeight + 'px';
-            cd.style.width = newHeight + 'px';
-            cd.style.opacity = newHeight / cdWidth;
+            cdImg.style.height = newHeight + 'px';
+            cdImg.style.width = newHeight + 'px';
+            cdImg.style.opacity = newHeight / cdWidth;
+        }
 
+        //Điều khiển nút Play
+        playBtn.onclick = function() {
+            _this.isPlaying = !_this.isPlaying;
+
+            if(_this.isPlaying) {
+                audio.play()
+                
+
+            }
+            else {
+                
+                audio.pause()
+            }
+
+        }
+
+        //Khi audio Chạy
+        
+        audio.onplaying = function() {
+            playBtn.classList.remove('pause')
+            playBtn.classList.add('playing')
+            
+            animateCd.play()
+            const duration = audio.duration
+            audio.ontimeupdate = function() {
+               
+                time.value = audio.currentTime / duration * 100;
+            }
+
+        }
+
+
+        //Khi audio dừng
+        audio.onpause = function() {
+            animateCd.pause()
+            playBtn.classList.remove('playing')
+            playBtn.classList.add('pause')
+        }
+
+        //Khi audio kết thúc
+        audio.onended = function() {
+            if(_this.isLoop) {
+                playBtn.click()
+                playBtn.click()
+                
+            }
+            if(_this.isRandom && !_this.isLoop) {
+
+                do {
+                    var randomIndex = Math.floor(Math.random() * _this.songs.length)
+
+                }
+                while(randomIndex === _this.currentIndex);
+                _this.currentIndex = randomIndex
+                _this.loadSongCurrent()
+                audio.play()
+
+            }
+        }
+
+        //Khi chỉnh sửa thời gian
+
+        time.onchange = function() {
+            const duration = audio.duration
+            audio.currentTime = time.value * duration / 100;
+            
+        }
+
+        //Khi ấn nút Lặp lại
+
+        loopBtn.onclick = function() {
+            _this.isLoop = !_this.isLoop;
+            loopBtn.classList.toggle('active', _this.isLoop)
             
         }
 
 
+        //Khi ấn nút next 
+        nextBtn.onclick = function() {
+            if(_this.isRandom) {
+                do {
+                    var randomIndex = Math.floor(Math.random() * _this.songs.length)
+
+                }
+                while(randomIndex === _this.currentIndex);
+                _this.currentIndex = randomIndex
+
+            }
+            else {
+                _this.currentIndex++
+                if(_this.currentIndex >= _this.songs.length) {
+                    _this.currentIndex = 0
+                }
+
+            }
+            _this.loadSongCurrent()
+            audio.play()
+        }
+
+        //Khi ấn nút prev 
+        prevBtn.onclick = function() {
+            _this.currentIndex--;
+            if(_this.currentIndex < 0) {
+                _this.currentIndex = _this.songs.length - 1;
+            }
+            _this.loadSongCurrent()
+            audio.play()
+        }
+
+        //Khi ấn nút random
+        randomBtn.onclick = function() {
+            _this.isRandom = !_this.isRandom
+            if(_this.isRandom) {
+                randomBtn.classList.add('active')
+            }
+            else {
+                randomBtn.classList.remove('active')
+            }
+        }
+
+        
+
+        
     },
-    
-    start: function(){
+    //Khi click chọn bài hát
+    handleClickItemSong : function() {
+        const songLists = $$('.music-item')
+        const _this = this
+        for(let i = 0; i < songLists.length ; i++) {
+            
+            songLists[i].onclick = function(e) {
+                
+                if(!songLists[i].classList.value.includes('active') && (e.target.localName !== 'i')) {
+                    
+                    _this.currentIndex = i
+                    _this.loadSongCurrent()
+                    audio.play()
+                }
+            }
+        }
 
-        this.handlerSelection(1)
 
-        this.handlerLoadSongs()
-    
-        this.handleScrollApp()
 
-        this.handleControlTime()
 
-        
+    },
 
-       
-    
-        
+
+    start: function() {
+        this.render()
+
+        this.handleEvents()
+
+        this.loadSongCurrent()
+
+        this.handleClickItemSong()
     }
+
 }
 
-
-
-
 app.start()
-
-
-// function getSongById(id) {
-//     var song = app.songs.find(s => s.id === id);
-
-//     return song
-
-// }
-
-// function handlerPlaying(id) {
-//     var song = getSongById(id);
-//     console.log(song)
-// }
-//Test
-
-// $('#audio').play()
-
-
-
-
-
-
-
-
